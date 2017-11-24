8b6a251927176a91de2146d5a5d2ac77765639d5
subotic <alex.subota.m@gmail.com>
2017-11-24 19:55:52 +0300

    First commit

--- MetricClient/MetricClient.py
+++ MetricClient/MetricClient.py
@@ -7,15 +7,20 @@ class Client():
 
     def put(self, name, val, timestamp = str(int(time.time()))):
         print ('put')
+        try:
+            self.client.send(name)
+        finally:
+            raise ClientError("PUT method got error")
 
     def get(self):
         print ('get')
 
 
-class ClientError():
-    def __init__(self):
-        pass
+class ClientError(Exception):
+    def __init__(self, message):
+        super(ClientError, self).__init__(message)
 
 
-myClient = Client('127.0.0.1',8080);
-myClient.put();
+raise ClientError("My hovercraft is full of eels")
+#myClient = Client('127.0.0.1',8080);
+#myClient.put();
